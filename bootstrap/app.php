737e9feb6882f7c692de952d1127ab092f32ae2c
RoyyertDev <royyertalfonso@gmail.com>
2025-03-09 18:34:45 -0400

    Change Kit Laravel by Jetstream

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,11 +9,17 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: [__DIR__.'/../routes/web.php', __DIR__.'/../routes/auth.php'],
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
         $middleware->encryptCookies(except: ['appearance']);
 
         $middleware->web(append: [
