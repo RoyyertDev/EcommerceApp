881229814c514b404f32527cf9b25b8842955657
rafaelb056 <rafaelbenitez056@gmail.com>
2025-03-11 18:39:07 -0400

    Implements Footer and Header

---
+++ resources/js/components/myComponents/LinksNav.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { usePage } from '@inertiajs/vue3';
+
+const props = defineProps({
+    route: {
+        type: String,
+    },
+});
+const active = (props.route == usePage().url);
+const classes = (props.route == usePage().url)
+            ? 'border-b-2 border-[#DE5976]'
+            : 'border-b-2 border-transparent group-hover:border-transparent transition duration-500 ease-out';
+</script>
+<template>
+
+    <Link v-if="active" :class="classes">
+        <slot />
+    </Link>
+    <div v-else class="relative group">
+        <Link  :class="classes">
+          <slot />
+        </Link>
+        <div class="absolute -bottom-.5 left-1/2 w-0 group-hover:w-full border-b-2 border-[#DE5976] transition-all duration-500 ease-out transform -translate-x-1/2"></div>
+    </div>
+</template>
---
+++ resources/js/layouts/landing/LandingLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import  Header from '@/components/land/Header.vue';
+  import  Footer from '@/components/land/Footer.vue';
+  defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+  })
+</script>
+<template>
+  <Header :canLogin="canLogin" :canRegister="canRegister" />
+  <slot />
+  <Footer />
+</template>
