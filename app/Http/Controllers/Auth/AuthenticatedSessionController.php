b3b3ea2a69b0d66ef2e7bb063f7d97fa9d6b8538
RoyyertDev <royyertalfonso@gmail.com>
2025-03-15 16:33:12 -0400

    Fixed redirect in login admin

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -36,7 +36,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route( Auth::user()->detail->role_id === RoleUser::where('name', 'admin')->first()->id ? 'admin.dashboard' : 'dashboard', absolute: false));
     }
 
     /**
