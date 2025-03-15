c6bacdc79a9e6a25bfa7e9819d2cc45041b96cf1
RoyyertDev <royyertalfonso@gmail.com>
2025-03-15 16:50:23 -0400

    Fix v7

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -36,7 +36,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->route('admin.dashboard');
+        return redirect()->intended(route('/'));
     }
 
     /**
